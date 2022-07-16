507a49be3da610f99e5d49d21d1708072c407d0f
Willian dos Santos <48397346+WilliandosSantos89@users.noreply.github.com>
2022-07-16 19:15:46 -0300

    erros encontrados

--- Dia7-Notifications-search-and-menu/index.js
+++ Dia7-Notifications-search-and-menu/index.js
@@ -1,9 +1,9 @@
 $('.search-icon').bind('click', function() {
-    $('search-input').toggleClass('active');
+    $('.search-input').toggleClass('active');
 });
 
 $('.menu-icon').bind('click', function() {
-    $('panel').toggleClass('show-menu');
-    $('menu').toggleClass('active');
+    $('.panel').toggleClass('show-menu');
+    $('.menu').toggleClass('active');
 });
 
